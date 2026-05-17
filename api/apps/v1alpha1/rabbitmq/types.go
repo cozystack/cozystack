7bbe8de02ebcf9e95630af14d84e7c49257dd2a6
kastakhov <16296930+kastakhov@users.noreply.github.com>
2026-05-17 10:44:13 +0000

    feat(rabbitmq): add affinity configuration
    Signed-off-by: kastakhov <16296930+kastakhov@users.noreply.github.com>

--- api/apps/v1alpha1/rabbitmq/types.go
+++ api/apps/v1alpha1/rabbitmq/types.go
@@ -7,6 +7,7 @@ package rabbitmq
 import (
 	resource "k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8sRuntime "k8s.io/apimachinery/pkg/runtime"
 )
 
 // +kubebuilder:object:root=true
@@ -36,6 +37,9 @@ type ConfigSpec struct {
 	// Enable external access from outside the cluster.
 	// +kubebuilder:default:=false
 	External bool `json:"external"`
+	// Pod affinity applied to RabbitMQ pods (standard Kubernetes corev1.Affinity). Use podAntiAffinity with topologyKey `kubernetes.io/hostname` to spread RabbitMQ pods across nodes.
+	// +kubebuilder:default:={}
+	Affinity *k8sRuntime.RawExtension `json:"affinity,omitempty"`
 	// RabbitMQ major.minor version to deploy
 	// +kubebuilder:default:="v4.2"
 	Version Version `json:"version"`
